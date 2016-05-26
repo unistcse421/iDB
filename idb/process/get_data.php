822f389cea4fdd0bec89912b53c9129e865e348c
Songhyun Kim <yeonmu_4121@naver.com>
2016-05-26 20:42:49 +0900

    get_data implemented

--- idb/process/get_data.php
+++ idb/process/get_data.php
@@ -1,4 +1,10 @@
 <?php
-    // get the data from microcontroller
-    // store data into db
+    include('../config/path.php');
+    include(CONFIG.'/db_config.php');
+    include(LIB.'/db.php');
+
+    session_start();
+    $mysqli = new mysqli($db['host'], $db['user'], $db['passwd'], $db['dbname']);
+
+    $mysqli->query("INSERT INTO data VALUES('{$_GET['id']}', '{$_GET['c']}', NOW(), '{$_GET['val']}')");
 ?>
