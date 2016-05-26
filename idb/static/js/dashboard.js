3bfe8373d947db99fb922b254cbaae496a0dee14
Sunhee Woo <wsh920319@gmail.com>
2016-05-26 14:44:38 +0900

    test3

--- idb/static/js/dashboard.js
+++ idb/static/js/dashboard.js
@@ -1,3 +1,11 @@
+var x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+var w = 200, h = 100;
+
+var svg = d3.select("article")
+            .append("svg")
+            .attr("width", w).attr("height", h);
+
+
 var margin = {top: 20, right: 80, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -92,12 +100,7 @@ d3.tsv(ROOT + "/static/js/data.tsv", function(error, data) {
 
 //-----------------------------------------------------------
 
-var x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-var w = 200, h = 100;
 
-var svg = d3.select("article")
-            .append("svg")
-            .attr("width", w).attr("height", h);
 
 svg.selectAll("rect")
     .data(x)
