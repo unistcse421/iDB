1e2bbbe0976a4f4dc2c8af6d8cb2aa8077831d88
Songhyun Kim <yeonmu_4121@naver.com>
2016-06-03 17:36:23 +0900

    json modify

--- web/process/echo_data.php
+++ web/process/echo_data.php
@@ -9,12 +9,14 @@
         $get_devices = $mysqli->query("SELECT device_id FROM device NATURAL JOIN consists WHERE machine_id='{$_GET['machine_id']}'");
         for($i=0; $i<$get_devices->num_rows; $i++) {
             $device = $get_devices->fetch_assoc();
-            $json[$i] = get_json_from_db($mysqli, "SELECT * FROM data WHERE device_id='{$device['device_id']}' ORDER BY created DESC LIMIT 100", true);
+            $json[$i] = "'{$device['device_id']}':".get_json_from_db($mysqli, "SELECT * FROM data WHERE device_id='{$device['device_id']}' ORDER BY created DESC LIMIT 100", true);
         }
 
+        echo '[';
         for($i=0; $i<count($json); $i++) {
-            echo $json[$i].'<br>';
+            echo $json[$i].',';
         }
+        echo ']';
     }
 
 ?>
